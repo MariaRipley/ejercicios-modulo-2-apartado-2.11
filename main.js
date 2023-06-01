2f335129fd3ddcf0d0b04340907d1443c4a9d6ab
MariaRipley <roblesb.maria@gmail.com>
2023-06-01 18:40:29 +0200

    Add practice 1 from lesson 2.11.1

---
+++ main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//Ejercicio 1
+
+let array100 = [];
+
+function get100Numbers() {
+   for (let i=1; i<101; i++ ){
+        // console.log(i);
+        let newLength = array100.push(i);
+    };   
+}
+
+get100Numbers();
+
+console.log(array100);
+
+//Ejercicio 2
+
+
+
+
+
